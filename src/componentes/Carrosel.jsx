8a306ee1d2f89e3b87f4cf771207f14e9e4c16d5
Marcos Coutinho <veniciusm4@gmail.com>
2025-02-04 15:10:04 -0300

    nova versão do projeto

--- src/componentes/Carrosel.jsx
+++ src/componentes/Carrosel.jsx
@@ -1,11 +1,18 @@
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function Carrosel(props) {
   const carossel = useRef(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (props.category && props.category.length > 0) {
+      setLoading(false);
+    }
+  }, [props.category]);
 
   const handleLeftClick = (e) => {
     e.preventDefault();
@@ -17,9 +24,9 @@ function Carrosel(props) {
     carossel.current.scrollLeft += carossel.current.offsetWidth;
   };
 
-  function SeeViewsClick(movie){
-    console.log(movie)
-    navigate('/movie/detail', {state:{movie}})
+  function SeeViewsClick(movie) {
+    console.log(movie);
+    navigate("/movie/detail", { state: { movie } });
   }
 
   return (
@@ -27,39 +34,50 @@ function Carrosel(props) {
       <h5 className="text-slate-50 text-xl pb-1 ml-11 font-bold">
         {props.nameCategory}
       </h5>
-      <div className="w-full flex gap-1 items-center">
-        <div>
-          <button className="text-slate-200" onClick={handleLeftClick}>
-            <ChevronLeft size={35} />
+      {loading ? (
+        <div className="w-full h-[399px] flex justify-center items-center">
+          <div className="animate-spin h-10 w-10 border-t-2 border-slate-200 rounded-full"></div>
+        </div>
+      ) : (
+        <div className="w-full flex gap-1 items-center">
+          <div className="">
+            <button className="text-slate-200" onClick={handleLeftClick}>
+              <ChevronLeft size={35} />
+            </button>
+          </div>
+          <div
+            className="carossel w-[97%] flex flex-nowrap gap-4 overflow-x-auto scroll-smooth overflow-y-hidden "
+            ref={carossel}
+          >
+            {props.category.map((movie) => (
+              <div
+                key={movie.id}
+                className="card h-[399px] min-w-[247px] pb-8 mb-1 "
+              >
+                <div className="image h-[100%] w-full relative group ">
+                  <div
+                    onClick={() => {
+                      SeeViewsClick(movie);
+                    }}
+                    className="box absolute top-0 left-0 right-0 bottom-0 cursor-pointer group-hover:bg-[rgba(0,0,0,0.4)] duration-100 ease-in-out"
+                  ></div>
+                  <img
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    alt={movie.title}
+                    className="h-full w-full object-cover rounded-md"
+                  />
+                  <span className="title text-slate-100 text-2xl font-bold ml-1 absolute top-3 left-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    {movie.title}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button className="" onClick={handleRightClick}>
+            <ChevronRight size={35} className="text-slate-200 h-16" />
           </button>
         </div>
-        <div
-          className="carossel w-[97%] flex flex-nowrap gap-4 overflow-x-auto scroll-smooth overflow-y-hidden "
-          ref={carossel}
-        >
-          {props.category.map((movie) => (
-            <div
-              key={movie.id}
-              className="card h-[399px] min-w-[247px] pb-8 mb-1 "
-            >
-              <div className="image h-[100%] w-full relative group ">
-                <div onClick={() => {SeeViewsClick(movie)}} className="box absolute top-0 left-0 right-0 bottom-0 cursor-pointer group-hover:bg-[rgba(0,0,0,0.4)] duration-100 ease-in-out"></div>
-                <img
-                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                  alt={movie.title}
-                  className="h-full w-full object-cover rounded-md"
-                />
-                <span className="title text-slate-100 text-2xl font-bold ml-1 absolute top-3 left-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  {movie.title}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
-        <button className="" onClick={handleRightClick}>
-          <ChevronRight size={35} className="text-slate-200 h-16"/>
-        </button>
-      </div>
+      )}
     </div>
   );
 }
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,16 +2,25 @@ import { useLocation } from "react-router-dom";
 import Navbar from "../componentes/Navbar.jsx";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function MoviesPage() {
   const location = useLocation();
-  const allMovies = location.state.movies;
-  const navigate = useNavigate()
+  const [allMovies, setAllMovies] = useState([]);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    function getMovies() {
+      const todayMovies = location.state?.movies || [];
+      setAllMovies(todayMovies);
+    }
 
-  function SeeViewsClick(movie){
-    console.log(movie)
-    navigate('/movie/detail', {state:{movie}})
+    getMovies();
+  }, [location.state]);
+
+  function SeeViewsClick(movie) {
+    console.log(movie);
+    navigate("/movie/detail", { state: { movie } });
   }
 
   return (
@@ -21,29 +30,32 @@ function MoviesPage() {
         <Navbar />
       </header>
       <div className="w-full py-5 px-8 flex gap-6 flex-wrap">
-        {allMovies.map((movie) => (
-          <div
-            key={movie.id}
-            className="card h-[399px] w-[247px]"
-          >
-            <div className="image h-[100%] w-full relative group">
-              <div
-                onClick={() => {
-                  SeeViewsClick(movie);
-                }}
-                className="box absolute top-0 left-0 right-0 bottom-0 cursor-pointer group-hover:bg-[rgba(0,0,0,0.4)] duration-100 ease-in-out"
-              ></div>
-              <img
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title}
-                className="h-full w-full object-cover rounded-md"
-              />
-              <span className="title text-slate-100 text-2xl font-bold ml-1 absolute top-3 left-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                {movie.title}
-              </span>
+        {Array.isArray(allMovies) && allMovies.length > 0 ? (
+          allMovies.map((movie) => (
+            <div key={movie.id} className="card h-[399px] w-[247px]">
+              <div className="image h-[100%] w-full relative group">
+                <div
+                  onClick={() => {
+                    SeeViewsClick(movie);
+                  }}
+                  className="box absolute top-0 left-0 right-0 bottom-0 cursor-pointer group-hover:bg-[rgba(0,0,0,0.4)] duration-100 ease-in-out"
+                ></div>
+                <img
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt={movie.title}
+                  className="h-full w-full object-cover rounded-md"
+                />
+                <span className="title text-slate-100 text-2xl font-bold ml-1 absolute top-3 left-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  {movie.title}
+                </span>
+              </div>
             </div>
+          ))
+        ) : (
+          <div className="w-full h-[399px] flex justify-center items-center">
+            <div className="animate-spin h-10 w-10 border-t-2 border-slate-200 rounded-full"></div>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
